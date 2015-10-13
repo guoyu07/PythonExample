bea3ec6c619c7266154aea19bd393bd79592578b
latexers <latexers@gmail.com>
2015-10-13 23:25:09 +0800

    QtUI

---
+++ GUI/WindowsExample.py
@@ -0,0 +1,12 @@
+#encoding = utf-8
+__author__ = 'Richard'
+
+import sys
+from PyQt4 import QtGui
+
+app = QtGui.QApplication(sys.argv)
+widget = QtGui.QWidget()
+widget.resize(250, 150)
+widget.setWindowTitle('PyQt')
+widget.show()
+sys.exit(app.exec_())
